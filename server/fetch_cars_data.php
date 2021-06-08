96849ad7e84c69cd7a70a3c42c5c676e612a2848
Darek <darekpl9@gmail.com>
2021-06-08 20:18:39 +0200

    changed cars fetching mode to assoc only

--- server/fetch_cars_data.php
+++ server/fetch_cars_data.php
@@ -6,7 +6,7 @@
     header("Access-Control-Allow-Origin: *");
 
     $stmt = $pdo->query("SELECT * FROM cars");
-    $cars_data = $stmt->fetchAll();
+    $cars_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo(json_encode($cars_data));
 ?>
