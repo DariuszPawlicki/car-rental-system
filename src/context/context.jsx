ec41c59c2686af06a21ff0cc4d2fe76a96f1939f
Jakub Pawelski <j.pawelski@interia.pl>
2021-06-10 13:32:10 +0200

    Change local state loggedIn

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -16,7 +16,7 @@ const initState = { rentalState: [] };
 export const RentalCarContext = createContext(initState);
 
 export const Provider = ({ children }) => {
-  const [loginResponse, setLoginResponse] = useState({ loggedIn: true });
+  const [loginResponse, setLoginResponse] = useState({ loggedIn: false });
 
   const [carModels, setCardModels] = useState([]);
 
