5c23850b0a5f4096feac703502d243c31b012246
Jakub Pawelski <j.pawelski@interia.pl>
2021-06-12 19:45:28 +0200

    Go to Heroku server

--- src/template/loginTemplate/loginTemplate.jsx
+++ src/template/loginTemplate/loginTemplate.jsx
@@ -19,8 +19,8 @@ const yupSchema = Yup.object({
     .required("Required !")
 });
 
-//export const API_URL = "https://cars-renting-server.herokuapp.com/";
-export const API_URL = "http://localhost:4000/server/";
+export const API_URL = "https://cars-renting-server.herokuapp.com/";
+// export const API_URL = "http://localhost:4000/server/";
 
 const LoginTemplate = () => {
   const { root, image, paper, avatar, form, error } = useStyles();
@@ -32,13 +32,13 @@ const LoginTemplate = () => {
     formDataPost.append("username", loginData["login"]);
     formDataPost.append("password", loginData["password"]);
 
-    fetch(API_URL + 'login.php', {
+    fetch(API_URL + "login.php", {
       method: "POST",
       credentials: "include",
       body: formDataPost
     })
-      .then((response) => response.json())
-      .then((data) => {
+      .then(response => response.json())
+      .then(data => {
         setLoginResponse({
           loggedIn: data["loggedIn"],
           message: data["message"],
