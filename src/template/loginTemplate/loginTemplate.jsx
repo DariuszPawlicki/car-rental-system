1c56b3db338ff3047f6baa224a57adc056d16be7
Dariusz Pawlicki <darekpl9gmail.com>
2022-05-28 12:09:55 +0200

    i

--- src/template/loginTemplate/loginTemplate.jsx
+++ src/template/loginTemplate/loginTemplate.jsx
@@ -21,8 +21,8 @@ const yupSchema = Yup.object({
 });
 
 //export const API_URL = "https://cars-renting-server.herokuapp.com/";
-//export const API_URL = "http://localhost:4000/server/";
-export const API_URL = "https://ec2-35-159-52-215.eu-central-1.compute.amazonaws.com/";
+export const API_URL = "http://localhost:4000/";
+//export const API_URL = "https://ec2-35-159-52-215.eu-central-1.compute.amazonaws.com/";
 
 const LoginTemplate = () => {
   const { root, image, paper, avatar, form, error } = useStyles();
