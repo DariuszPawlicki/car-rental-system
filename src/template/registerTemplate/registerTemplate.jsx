ab61cf50f1e5a96fc53574c6192394f2c8792d45
Dariusz Pawlicki <darekpl9gmail.com>
2022-05-28 12:08:55 +0200

    fix

--- src/template/registerTemplate/registerTemplate.jsx
+++ src/template/registerTemplate/registerTemplate.jsx
@@ -25,7 +25,8 @@ const yupSchema = Yup.object({
 });
 
 //export const API_URL = "https://cars-renting-server.herokuapp.com/";
-export const API_URL = "http://localhost:4000/server/";
+//export const API_URL = "http://localhost:4000/server/";
+export const API_URL = "https://ec2-35-159-52-215.eu-central-1.compute.amazonaws.com/";
 
 const RegisterTemplate = () => {
   const { root, image, paper, avatar, form, error } = useStyles();
