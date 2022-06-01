66305640557663ccec10514fd876216949c7e545
Jakub Pawelski <j.pawelski@interia.pl>
2022-06-01 13:23:51 +0200

    delete logic condition

--- src/template/registerTemplate/registerTemplate.jsx
+++ src/template/registerTemplate/registerTemplate.jsx
@@ -49,10 +49,7 @@ const RegisterTemplate = () => {
     })
       .then(response => response.json())
       .then(data => {
-        console.log("data", data);
-        if (data) {
-          history.push("/login");
-        }
+        history.push("/login");
       });
   };
 
